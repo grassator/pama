916fb79864f86807d314e91349054e439c1b7ada
Dmitriy Kubyshkin <grassator@gmail.com>
2017-03-25 15:37:33 +0100

    A much simpler implementation as a conditional expression

--- index.js
+++ index.js
@@ -11,402 +11,76 @@
 
     'use strict';
 
+    /**
+     * @param {*} x
+     * @returns {*}
+     */
     function id(x) {
         return x;
     }
 
-    function constant(value) {
-        return function () {
-            return value;
-        };
-    }
-
-    function alwaysTrue() { return true; }
-
-    function error(msg) { throw new Error(msg); }
-
-    function Undefined() {
+    /**
+     * @param {*} x
+     * @param {*} y
+     * @returns {boolean}
+     */
+    function eq(x, y) {
+        return x === y;
     }
 
     /**
-     * @param {Function} type
-     * @param {*} pattern
-     * @constructor
+     * @returns {boolean}
      */
-    function PatternMatcher(type, pattern) {
-        this.type = type;
-        this.pattern = pattern;
-        this.callback = id;
-        this.guards = null;
-        this.storage = null;
+    function any() {
+        return true;
     }
 
-    /**
-     * @param {Function=} predicate
-     * @returns {PatternMatcher}
-     */
-    PatternMatcher.prototype['if'] = function (predicate) {
-        predicate = predicate || alwaysTrue;
-        if (this.guards === null) {
-            this.guards = [];
+    function type(thing, classOrType) {
+        if (typeof classOrType === 'string') {
+            return typeof thing === classOrType;
         }
-        this.guards.push(new Guard(predicate));
-        return this;
-    };
-
-    /**
-     * @param {Function|*} callback
-     * @returns {PatternMatcher}
-     */
-    PatternMatcher.prototype['then'] = function (callback) {
-        callback = (typeof callback === 'function') ?
-            callback : constant(callback);
-        if (this.guards !== null) {
-            this.guards[this.guards.length - 1].callback = callback;
-        } else {
-            this.callback = callback;
-        }
-        return this;
-    };
-
-    /**
-     * @param {Function} callback
-     * @returns {PatternMatcher}
-     */
-    PatternMatcher.prototype['otherwise'] = function (callback) {
-        return this.if().then(callback);
-    };
-
-    /**
-     * @param {*=} type
-     * @param {*=} pattern
-     * @returns {PatternMatcher}
-     */
-    function is(type, pattern) {
-        if (arguments.length === 0) {
-            type = pattern = any;
-        } else if (typeof type !== 'function') {
-            pattern = type;
-            type = patternToType(pattern);
-        } else if (arguments.length === 1) {
-            pattern = any;
-        }
-        return new PatternMatcher(type, pattern);
+        return thing instanceof classOrType;
     }
 
-    /**
-     * @param {*=} type
-     * @param {*=} pattern
-     * @returns {PatternMatcher}
-     */
-    function otherwise(type, pattern) { // eslint-disable-line no-unused-vars
-        const matcher = is();
-        return matcher['then'].apply(matcher, arguments);
-    }
-
-    /**
-     * @param {Function=} predicate
-     * @returns {PatternMatcher}
-     */
-    otherwise['if'] = function (predicate) {
-        return is()['if'](predicate);
-    };
-
-    /**
-     * @param {*} name
-     * @param {*} matcher
-     * @constructor
-     */
-    function Capture(name, matcher) {
-        this.name = name;
-        this.matcher = matcher === undefined ? any : matcher;
-        this.isRest = false;
-    }
-
-    /**
-     * @param {*} nameOrSubPattern
-     * @param {*=} subPatternOrNothing
-     * @returns {Capture}
-     */
-    function createCapture(nameOrSubPattern, subPatternOrNothing) {
-        if (arguments.length === 1) {
-            if (typeof nameOrSubPattern === 'string') {
-                return new Capture(nameOrSubPattern);
-            } else {
-                return new Capture(undefined, nameOrSubPattern);
-            }
-        } else {
-            return new Capture(nameOrSubPattern, subPatternOrNothing);
+    function matches(value, pattern) {
+        if (pattern === any) {
+            return true;
         }
-    }
-
-    createCapture.rest = remainingArrayItems;
-
-    var any = createCapture;
-
-    /**
-     * @param {string=} name
-     * @return {Capture}
-     */
-    function remainingArrayItems(name) {
-        var capture = new Capture(name);
-        capture.isRest = true;
-        return capture;
-    }
-
-    /**
-     * @param {*} value
-     * @constructor
-     */
-    function CallbackArgs(value) {
-        this.value = value;
-        this.storage = null;
-    }
-
-    /**
-     * @param {*} value
-     * @param {*} name
-     */
-    CallbackArgs.prototype.push = function (value, name) {
-        if (name !== undefined) {
-            if (this.storage === null) {
-                this.storage = {};
-            }
-            this.storage[name] = value;
-        } else {
-            if (this.storage === null) {
-                this.storage = [];
-            }
-            this.storage.push(value);
-        }
-    };
-
-    /**
-     * @returns {Array}
-     */
-    CallbackArgs.prototype.toArray = function () {
-        if (this.storage === null) {
-            return [this.value];
-        }
-        if (Array.isArray(this.storage)) {
-            return this.storage;
-        }
-        return [this.storage];
-    };
-
-    /**
-     * @param {Function} predicate
-     * @constructor
-     */
-    function Guard(predicate) {
-        this.predicate = predicate;
-        this.callback = id;
-    }
-
-
-    /**
-     * @param {*} pattern
-     * @returns {Function}
-     */
-    function patternToType(pattern) {
-        switch (typeof pattern) {
-            case 'undefined':
-                return Undefined;
-            case 'number':
-                return Number;
-            case 'boolean':
-                return Boolean;
-            case 'string':
-                return String;
-            default:
-                return Object;
-        }
-    }
-
-    /**
-     * @param {*} value
-     * @param {PatternMatcher} matcher
-     * @param {CallbackArgs} storage
-     */
-    function doMatch(value, matcher, storage) {
-        var typeOfValue = typeof value;
-        var pattern = matcher.pattern;
-        var callback = matcher.callback;
-        var type = matcher.type;
-        switch (type) {
-            case any:
-                break;
-            case Boolean:
-                if (typeOfValue !== 'boolean') {
-                    return null;
-                }
-                break;
-            case Number:
-                if (typeOfValue !== 'number') {
-                    return null;
-                }
-                break;
-            case String:
-                if (typeOfValue !== 'string') {
-                    return null;
-                }
-                break;
-            case Undefined:
-                if (typeOfValue !== 'undefined') {
-                    return null;
-                }
-                break;
-            case Object:
-                if (typeOfValue !== 'object') {
-                    return null;
-                }
-                break;
-            default:
-                if (!(value instanceof type)) {
-                    return null;
-                }
-                break;
-        }
-        var guards = matcher.guards;
-        var isMatch = false;
-        if (pattern !== any) {
-            switch (matcher.type) {
-                case Number:
-                    if (isNaN(value) && isNaN(pattern)) {
-                        isMatch = true;
-                        break;
-                    }
-                /* fallthrough */
-                case Boolean:
-                case String:
-                case Undefined:
-                    isMatch = value === pattern;
-                    break;
-                default:
-                    isMatch = pattern === null ?
-                        pattern === value :
-                        doMatchObject(value, pattern, storage);
-                    break;
-            }
-        } else {
-            isMatch = true;
-        }
-        if (isMatch && guards !== null) {
-            for (var i = 0; i < guards.length; ++i) {
-                if (guards[i].predicate(value) === true) {
-                    return guards[i].callback;
-                }
-            }
-            return null;
-        }
-        return isMatch ? callback : null;
-    }
-
-
-    /**
-     * @param {*} value
-     * @param {*} pattern
-     * @param {CallbackArgs} storage
-     */
-    function doMatchObjectInternal(value, pattern, storage) {
-        var capture = null;
-        var result = true;
-        if (pattern instanceof Capture) {
-            capture = pattern;
-            pattern = capture.matcher;
-        }
-        if (pattern !== any) {
-            if (!(pattern instanceof PatternMatcher)) {
-                pattern = new PatternMatcher(patternToType(pattern), pattern);
-            }
-            result = doMatch(value, pattern, storage) !== null;
-        }
-        if (result && capture !== null) {
-            storage.push(value, capture.name);
-        }
-        return result;
-    }
-
-    /**
-     * @param {*} value
-     * @param {*} pattern
-     * @param {CallbackArgs} storage
-     */
-    function doMatchObject(value, pattern, storage) {
-        if (value === null) {
+        if (typeof pattern !== 'object' || pattern === null) {
             return value === pattern;
         }
-        if (Array.isArray(pattern)) {
-            if (!Array.isArray(value)) {
-                return false;
-            }
-            var hasRestMatch = false;
-            var lastItem;
-            if (pattern.length > 0) {
-                lastItem = pattern[pattern.length - 1];
-                if (lastItem === remainingArrayItems || (lastItem && lastItem.isRest)) {
-                    hasRestMatch = true;
-                }
-            }
-            if (!hasRestMatch && pattern.length !== value.length) {
-                return false;
-            }
-            for (var i = 0; i < pattern.length; ++i) {
-                if (i === pattern.length - 1 && hasRestMatch) {
-                    if (lastItem === remainingArrayItems) {
-                        return true;
-                    } else {
-                        return doMatchObjectInternal(value.slice(i), pattern[i], storage);
-                    }
-                }
-                if (!doMatchObjectInternal(value[i], pattern[i], storage)) {
+        if (typeof value !== 'object') {
+            return false;
+        }
+        for (var prop in pattern) {
+            if (pattern.hasOwnProperty(prop)) {
+                if (!(prop in value) || !matches(value[prop], pattern[prop])) {
                     return false;
-                }
-            }
-        } else {
-            for (var key in pattern) {
-                if (pattern.hasOwnProperty(key)) {
-                    if (!(key in value)) {
-                        return false;
-                    }
-                    if (!doMatchObjectInternal(value[key], pattern[key], storage)) {
-                        return false;
-                    }
                 }
             }
         }
         return true;
     }
 
-    exports['is'] = is;
-
-    function when (value, varArgs) { // eslint-disable-line no-unused-vars
-        if (value instanceof PatternMatcher) {
-            var args = Array.prototype.slice.call(arguments);
+    function when (value, callback) {
+        if (callback === undefined) {
+            callback = value;
             return function (value) {
-                return when.apply(undefined, [value].concat(args));
+                return when(value, callback);
             };
         }
-        if (arguments.length < 2) {
-            error('You need to provide at least one matcher');
+        const result = callback({
+            _: any,
+            id: id,
+            eq: eq.bind(undefined, value),
+            type: type.bind(undefined, value),
+            matches: matches.bind(undefined, value)
+        }, value);
+        if (typeof result === 'function') {
+            return result(value);
         }
-        for (var i = 1, callback, storage; i < arguments.length; ++i) {
-            storage = new CallbackArgs(value);
-            if ((callback = doMatch(value, arguments[i], storage))) {
-                return callback.apply(undefined, storage.toArray());
-            }
-        }
+        return result;
     }
 
-    /**
-     * @param {*} value
-     * @param {...PatternMatcher} varArgs
-     * @returns {*}
-     */
-    exports['when'] = when;
-
-    exports['_'] = exports['any'] = any;
-    exports['otherwise'] = otherwise;
-
-    return exports;
+    return when;
 }));
--- index.test.js
+++ index.test.js
@@ -1,180 +1,84 @@
 /* global test, expect */
 
-const {when, is, otherwise, any:_} = require('./index');
+/* eslint-disable no-unexpected-multiline */
 
-test('throws when there are no matchers are provided', () => {
-    expect(() => when({}))
-        .toThrow(Error);
-});
+const when = require('./index');
 
 test('works for matching boolean values', () => {
-    expect(when(false,
-        is(true).then('true'),
-        is(false).then('false')
+    expect(when(false, ({eq}) =>
+        eq(true) ? 'true' :
+        eq(false) ? 'false' :
+        undefined
     )).toBe('false');
 });
 
-test('just returns the value if there is no callback for a match', () => {
-    expect(when(false,
-        is(true).then('true'),
-        is(false)
+test('to have an id callback', () => {
+    expect(when(false, ({eq, id}) =>
+        eq(true) ? 'true' :
+        eq(false) ? id :
+        undefined
     )).toBe(false);
-    expect(when(true,
-        is(true),
-        is(false).then('false')
-    )).toBe(true);
 });
 
 test('works for matching number', () => {
-    expect(when(42,
-        is(0),
-        is(42),
-        is(79)
+    expect(when(42, ({eq, id}) =>
+        eq(0) ? id :
+        eq(42) ? id :
+        eq(79) ? id :
+        undefined
     )).toBe(42);
-    expect(when(Infinity,
-        is(0),
-        is(Infinity),
-        is(79)
+    expect(when(Infinity, ({eq, id}) =>
+        eq(0) ? id :
+        eq(Infinity) ? id :
+        eq(79) ? id :
+        undefined
     )).toBe(Infinity);
-    expect(when(-0,
-        is(0),
-        is(-0),
-        is(79)
-    )).toBe(-0);
-    expect(Number.isNaN(when(NaN,
-        is(0),
-        is(NaN),
-        is(79)
-    ))).toBe(true);
-    expect(when(42,
-        is(String).then('string'),
-        is(Number).then('number')
+    expect(when(Infinity, ({type}) =>
+        type('string') ? 'string' :
+        type('number') ? 'number' :
+        undefined
     )).toBe('number');
 });
 
-test('works for matching strings', () => {
-    expect(when('foo',
-        is('foo'),
-        is('bar')
-    )).toBe('foo');
-    expect(when('',
-        is('foo'),
-        is('')
-    )).toBe('');
-    expect(when('',
-        is(String).then('string'),
-        is(Number).then('number')
-    )).toBe('string');
-});
-
-test('works for matching regular expressions', () => {
-    class Foo {}
-    expect(when(/foo/,
-        is(RegExp, { source: 'bar' } ).then('bar regexp'),
-        is(RegExp, { source: 'foo', flags: 'g' }).then('foo regexp but global'),
-        is(Foo, { source: 'foo' }).then('foo class'),
-        is(RegExp, { source: 'foo' }).then('foo regexp')
-    )).toBe('foo regexp');
-    expect(when(/foo/,
-        is(RegExp).then('regexp'),
-        is(String).then('string')
-    )).toBe('regexp');
-});
-
 test('works for null', () => {
-    expect(when(null,
-        is(undefined).then('undefined'),
-        is(false).then('false'),
-        is(null).then('null')
+    expect(when(null, ({eq}) =>
+        eq(undefined) ? 'undefined' :
+        eq(false) ? 'false' :
+        eq(null) ? 'null' :
+        undefined
     )).toBe('null');
-    expect(when(null,
-        is(undefined).then('undefined'),
-        is(false).then('false'),
-        is(Object).then('object')
-    )).toBe('object');
-});
-
-test('does not match any object as null', () => {
-    expect(when({},
-        is(null).then('null'),
-        is(false).then('false'),
-        is(Object).then('object')
+    expect(when(null, ({eq, type}) =>
+        eq(undefined) ? 'undefined' :
+        eq(false) ? 'false' :
+        type('object') ? 'object' :
+        undefined
     )).toBe('object');
 });
 
 test('works for undefined', () => {
-    expect(when(undefined,
-        is(false).then('false'),
-        is(null).then('null'),
-        is(undefined).then('undefined')
+    expect(when(undefined, ({eq}) =>
+        eq(false) ? 'false' :
+        eq(undefined) ? 'undefined' :
+        eq(null) ? 'null' :
+        undefined
     )).toBe('undefined');
 });
 
 test('works for custom classes', () => {
     class Foo {}
     class Bar {}
-    expect(when(new Foo(),
-        is(Bar).then('Bar'),
-        is(Foo).then('Foo'),
-        is(Object).then('object')
+    expect(when(new Foo(), ({type}) =>
+        type(Bar) ? 'Bar' :
+        type(Foo) ? 'Foo' :
+        undefined
     )).toBe('Foo');
 });
 
 test('works for matching anything', () => {
-    expect(when(9,
-        is(42).then('the answer'),
-        otherwise('not the answer')
+    expect(when(9, ({eq}) =>
+        eq(42) ? 'the answer' :
+        'not the answer'
     )).toBe('not the answer');
-});
-
-test('works for matching with custom predicates', () => {
-    expect(when(42,
-        otherwise
-            .if(x => x < 0).then('negative')
-            .if(x => x >= 0).then('positive')
-    )).toBe('positive');
-    expect(when(42,
-        is(Number)
-            .if(x => x < 0).then('negative')
-            .if(x => x >= 0).then('positive')
-    )).toBe('positive');
-    expect(when(42,
-        is(Number, 42)
-            .if(x => x < 0).then('negative')
-            .if(x => x >= 0).then('positive')
-    )).toBe('positive');
-});
-
-test('works for matching with regular predicates and predicates with guards', () => {
-    expect(when(42,
-        is().if(x => x < 0).then('negative'),
-        is().then('positive')
-    )).toBe('positive');
-});
-
-test('works for empty guards', () => {
-    expect(when(42,
-        is()
-            .if(x => x < 0).then('negative')
-            .if().then('positive')
-    )).toBe('positive');
-});
-
-test('supports `otherwise` as a alias for an empty guard with `then`', () => {
-    expect(when(42,
-        is()
-            .if(x => x < 0).then('negative')
-            .otherwise('positive')
-    )).toBe('positive');
-});
-
-test('correctly applies original condition to the guards', () => {
-    expect(when({ type: 'foo' },
-        is({ type: 'bar' })
-            .if(o => Object.keys(o).length > 5).then('big object')
-            .otherwise('small object'),
-        otherwise('string')
-    )).toBe('string');
 });
 
 test('works for matching props on an object', () => {
@@ -183,29 +87,19 @@ test('works for matching props on an object', () => {
             this.foo = 'bar';
         }
     }
-    expect(when(new Foo(),
-        is(Foo, {foo: 'foo'}).then('foo'),
-        is(Foo, {foo: 'bar'}).then('bar')
+    expect(when(new Foo(), ({matches}) =>
+        matches({foo: 'foo'}) ? 'foo' :
+        matches({foo: 'bar'}) ? 'bar' :
+        undefined
     )).toBe('bar');
 });
 
 test('`any` match should check for the presence of property on the object', () => {
-    expect(when({},
-        is({foo: _}).then('foo'),
-        is({}).then('empty')
+    expect(when({}, ({matches, _}) =>
+        matches({foo: _}) ? 'foo' :
+        matches({}) ? 'empty' :
+        undefined
     )).toBe('empty');
-});
-
-test('works for matching props on an object without class', () => {
-    class Foo {
-        constructor() {
-            this.foo = 'bar';
-        }
-    }
-    expect(when(new Foo(),
-        is({foo: 'foo'}).then('foo'),
-        is({foo: 'bar'}).then('bar')
-    )).toBe('bar');
 });
 
 test('works for deep matching', () => {
@@ -218,78 +112,25 @@ test('works for deep matching', () => {
             }
         }
     }
-    expect(when(new Foo(),
-        is(Foo, {foo: 'foo'}).then('foo'),
-        is(Foo, {foo: {foo:{foo: 'bar'}}}).then('bar')
+    expect(when(new Foo(), ({matches}) =>
+        matches({foo: 'foo'}) ? 'foo' :
+        matches({foo: {foo: {foo: 'bar'}}}) ? 'bar' :
+        undefined
     )).toBe('bar');
-});
-
-test('works for capturing deep nested values', () => {
-    class Foo {
-        constructor(depth = 0) {
-            if (depth < 2) {
-                this.foo = new Foo(depth + 1);
-            } else {
-                this.foo = 'bar';
-            }
-        }
-    }
-    expect(when(new Foo(),
-        is(Foo, {foo: 'foo'}).then('foo'),
-        is(Foo, {foo: {foo:{foo: _()}}}).then(x => x)
-    )).toBe('bar');
-});
-
-test('works for capturing multiple values', () => {
-    expect(when({ foo: 'foo', bar: 'bar'},
-        is(Object, {foo: _(), bar: _()}).then((x, y) => [x, y]),
-        is(Object).then('object')
-    )).toEqual(['foo', 'bar']);
-});
-
-test('works for capturing named values', () => {
-    expect(when({ foo: 'foo', bar: 'bar'},
-        is(Object, {foo: _('x'), bar: _}).then(({x}) => x),
-        is(Object).then('object')
-    )).toEqual('foo');
 });
 
 test('works for matching shallow arrays as objects', () => {
-    expect(when(['foo', 42],
-        is(['foo']).then('foo'),
-        is(['foo', 42]).then('foo 42'),
-        is(Array).then('Array')
-    )).toEqual('foo 42');
-});
-
-test('works for capturing a nested match', () => {
-    expect(when(['foo', { bar: 42 }],
-        is(['foo', _(is({ bar: 42 }))]).then(x => x),
-        is(Array).then('Array')
-    )).toEqual({ bar: 42 });
-});
-
-test('works for capturing inside a nested match', () => {
-    expect(when(['foo', { bar: 42 }],
-        is(['foo', is(Object, { bar: _() })]).then(x => x),
-        is(Array).then('Array')
-    )).toEqual(42);
-});
-
-test('works for matching rest elements of the array', () => {
-    expect(when(['foo', 42],
-        is(['foo', _.rest]).then('foo'),
-        is(Array).then('Array')
-    )).toEqual('foo');
-});
-
-test('works for capturing rest elements of the array', () => {
-    expect(when(['foo', 42, 43],
-        is(['foo', _.rest()]),
-        is(Array).then('Array')
-    )).toEqual([42, 43]);
+    expect(when(['foo', 42], ({matches, type}) =>
+        matches(['foo', 9]) ? 'foo 9' :
+        matches(['foo', 42]) ? 'foo 42' :
+        type(Array) ? 'Array' :
+        undefined
+    )).toBe('foo 42');
 });
 
 test('allows to auto-create a function when a an element to match is not provided', () => {
-    expect(when(is(true))).toBeInstanceOf(Function);
+    expect(when(({eq}) =>
+        eq(true) ? 'true' :
+        undefined
+    )).toBeInstanceOf(Function);
 });
